b085cb5d0f870fa240162797dbe12351d15d70f9
Franziskus Domig <franziskus.domig@comsysto.com>
2012-11-07 23:43:59 +0100

    first version

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var
+  dyndns = require('../lib/dyndns'),
+  timer  = require('../lib/time');
+
+/*
+ * GET home page.
+ */
+
+var
+  last_ip     = null,
+  last_change = new Date();
+
+exports.index = function(req, res) {
+  dyndns.my_ip(function(err, ip) {
+    if (err) throw err;
+
+    if (last_ip && last_ip != ip) {
+      last_change = new Date()
+      last_ip = ip;
+      uptime = 0;
+    } else {
+      uptime = timer.since(last_change);
+    }
+
+    res.render('index', {
+      title: 'dynDNS',
+      my_ip: ip,
+      uptime: uptime
+    });
+  });
+};
